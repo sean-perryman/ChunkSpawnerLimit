a740778ab0f013fb0f69c86da4552547ad3d8daf
PhanaticD <javw.phanatic@gmail.com>
2017-06-03 04:42:28 -0400

    make sure its mob spawner heh heh

--- com/phanaticmc/chunkspawnerlimit/listeners/BlockPlace.java
+++ com/phanaticmc/chunkspawnerlimit/listeners/BlockPlace.java
@@ -4,6 +4,7 @@ import static com.phanaticmc.chunkspawnerlimit.ChunkSpawnerLimit.instance;
 import static com.phanaticmc.chunkspawnerlimit.ChunkSpawnerLimit.limit;
 import static org.bukkit.Bukkit.getServer;
 import org.bukkit.Chunk;
+import org.bukkit.Material;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.event.EventHandler;
@@ -14,6 +15,9 @@ import org.bukkit.event.block.BlockPlaceEvent;
 public class BlockPlace implements Listener {
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onBlockPlace(BlockPlaceEvent e){
+        if(e.getBlock().getType() != Material.MOB_SPAWNER){
+            return;
+        }
         Chunk c = e.getBlock().getLocation().getChunk();
         int spawnercount = 1;
         for (BlockState block : c.getTileEntities()) {
